1c0da4615db6b728223efbb172aaa930ec980a12
Pranav <pranav.dilip.patel@gmail.com>
2018-10-02 18:22:48 +0200

    new change

---
+++ src/Assignment1/MyIntegerBST.java
@@ -0,0 +1,28 @@
+package Assignment1;
+
+public class MyIntegerBST implements A1Tree {
+
+	@Override
+	public void insert(Integer value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Integer mostSimilarValue(Integer value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printByLevels() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
