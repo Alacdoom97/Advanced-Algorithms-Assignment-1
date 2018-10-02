02e59907f7ac488aeba2e74bdde9f31444af8359
Pranav <pranav.dilip.patel@gmail.com>
2018-10-02 19:47:17 +0200

    updated insert method

--- src/Assignment1/MyIntegerBST.java
+++ src/Assignment1/MyIntegerBST.java
@@ -2,9 +2,39 @@ package Assignment1;
 
 public class MyIntegerBST implements A1Tree {
 
+	private Node root = null;
+
+	public MyIntegerBST() {
+		this.root = null;
+	}
+
 	@Override
 	public void insert(Integer value) {
-		// TODO Auto-generated method stub
+		Node addNode = new Node(value);
+
+		if (root == null) {
+			root = addNode;
+			return;
+		} else if (!containsNode(value)) {
+			Node now = root;
+			Node parent = null;
+			while (true) {
+				parent = now;
+				if (value < now.key) {
+				}
+				now = now.left;
+				if (now == null) {
+					parent.left = addNode;
+					return;
+
+				} else if (value > now.key) {
+					parent.right = addNode;
+				}
+			}
+		}
+		else {
+			System.err.println("You have inserted a duplicate integer, please insert another integer.");
+		}
 
 	}
 
@@ -20,9 +50,37 @@ public class MyIntegerBST implements A1Tree {
 
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Node searchForNode(Node searchNode, Integer key) {
+		if (searchNode == null) {
+			return null;
+		} else {
+			if (searchNode.key > key) {
+				searchForNode(searchNode.left, key);
+			} else if (searchNode.key < key) {
+				searchForNode(searchNode.right, key);
+			}
+			return searchNode;
+		}
 
+	}
+
+	public boolean containsNode(Integer key) {
+		if (searchForNode(root, key).key == key)
+			return true;
+		else
+			return false;
+	}
+
+	private class Node {
+		Integer key;
+		Node left;
+		Node right;
+
+		public Node(Integer key) {
+			this.key = key;
+			left = null;
+			right = null;
+		}
 	}
 
 }
