8be240dc94ac0b19bce99d3e442b8d935717cd66
Pranav <pranav.dilip.patel@gmail.com>
2018-10-05 17:12:39 +0200

    exercise 3 start

--- src/Assignment1/A1SequenceWithMinimum.java
+++ src/Assignment1/A1SequenceWithMinimum.java
@@ -1,7 +1,7 @@
 package Assignment1;
 
 public interface A1SequenceWithMinimum {
-        public void insertRight(Integer value); 
+    public void insertRight(Integer value); 
 	public Integer removeRight();
 	public void insertLeft(Integer value);
 	public Integer removeLeft();
---
+++ src/Assignment1/MySequenceADT.java
@@ -0,0 +1,40 @@
+package Assignment1;
+
+public class MySequenceADT implements A1SequenceWithMinimum {
+
+	@Override
+	public void insertRight(Integer value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Integer removeRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertLeft(Integer value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Integer removeLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer findMinimum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
